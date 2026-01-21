70e6e81136bc737f7784ab6eee63ab5fa715f499
ivanmaulana1541 <ivan.maulana1541@gmail.com>
2026-01-21 12:08:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // STATE
   let syabilOutfit = "piyama";
+
+  const sceneRoom = document.getElementById("scene-room");
+  const sceneKitchen = document.getElementById("scene-kitchen");
 
   const syabilImg = document.getElementById("syabil-img");
   const dialogText = document.getElementById("dialog-text");
   const btnChange = document.getElementById("btn-change");
+  const doorRoom = document.getElementById("door-room");
 
   btnChange.addEventListener("click", () => {
 
     if (syabilOutfit === "piyama") {
 
-      // loncat
       syabilImg.classList.add("jump");
 
       setTimeout(() => {
-        // ganti baju
         syabilOutfit = "school";
         syabilImg.src = "assets/child.png";
       }, 150);
 
       setTimeout(() => {
-        // turun lagi
         syabilImg.classList.remove("jump");
 
         dialogText.textContent =
           "Syabil sudah siap. Saatnya keluar kamar.";
 
         btnChange.style.display = "none";
+        doorRoom.classList.remove("hidden");
       }, 300);
     }
+  });
 
+  doorRoom.addEventListener("click", () => {
+    sceneRoom.classList.remove("active");
+    sceneKitchen.classList.add("active");
+
+    dialogText.textContent =
+      "Mama dan Papa sudah menunggu di dapur.";
   });
 
 });
