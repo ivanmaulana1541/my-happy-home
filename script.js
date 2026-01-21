880f57392dc7ba7180353e04cf7f946ebc079ca7
ivanmaulana1541 <ivan.maulana1541@gmail.com>
2026-01-21 12:30:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  // ===== STATE AWAL =====
   let syabilOutfit = "piyama";
 
   const syabilImg = document.getElementById("syabil-img");
   const dialogText = document.getElementById("dialog-text");
   const btnChange = document.getElementById("btn-change");
-  const door = document.getElementById("door");
 
-  // GANTI BAJU
+  // ===== SET AWAL (PENTING) =====
+  syabilImg.src = "assets/piyama.png";
+  dialogText.textContent = "Syabil masih memakai piyama. Ia harus ganti baju dulu.";
+
+  // ===== GANTI BAJU =====
   btnChange.addEventListener("click", () => {
     if (syabilOutfit === "piyama") {
       syabilOutfit = "school";
 
-      // efek loncat
       syabilImg.classList.add("jump");
 
       setTimeout(() => {
+        syabilImg.src = "assets/child.png";
         syabilImg.classList.remove("jump");
 
-        dialogText.textContent =
-          "Syabil sudah siap. Saatnya keluar kamar.";
-
+        dialogText.textContent = "Syabil sudah siap. Saatnya keluar kamar.";
         btnChange.style.display = "none";
-        door.classList.remove("hidden");
-      }, 400);
+      }, 300);
     }
-  });
-
-  // CLICK PINTU
-  door.addEventListener("click", () => {
-    alert("Menuju Kitchen (lanjut tahap berikutnya)");
   });
 
 });
