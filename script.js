a2e0df5ade4ee47f91ddeb4ccad361cb61aa349f
ivanmaulana1541 <ivan.maulana1541@gmail.com>
2026-01-21 12:20:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const door = document.getElementById("door");
-  const bg = document.getElementById("bg");
+
+  let syabilOutfit = "piyama";
+
+  const syabilImg = document.getElementById("syabil-img");
   const dialogText = document.getElementById("dialog-text");
+  const btnChange = document.getElementById("btn-change");
+  const door = document.getElementById("door");
 
-  door.addEventListener("click", () => {
-    // pindah ke kitchen
-    bg.src = "assets/background/kitchen.png";
-    dialogText.textContent = "Mama dan Papa sudah menunggu di dapur.";
+  // GANTI BAJU
+  btnChange.addEventListener("click", () => {
+    if (syabilOutfit === "piyama") {
+      syabilOutfit = "school";
 
-    // opsional: sembunyikan pintu
-    door.style.display = "none";
+      // efek loncat
+      syabilImg.classList.add("jump");
+
+      setTimeout(() => {
+        syabilImg.classList.remove("jump");
+
+        dialogText.textContent =
+          "Syabil sudah siap. Saatnya keluar kamar.";
+
+        btnChange.style.display = "none";
+        door.classList.remove("hidden");
+      }, 400);
+    }
   });
+
+  // CLICK PINTU
+  door.addEventListener("click", () => {
+    alert("Menuju Kitchen (lanjut tahap berikutnya)");
+  });
+
 });
